4a9311a991ebece097a71b873d897457dce8fd49
KARTHIKA  N <karthikanagaraj2006@gmail.com>
2026-07-20 22:35:04 +0530

    Create multidimensional Array .c

---
+++ multidimensional Array .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int r,c,i;
+scanf("%d%d",&r,&c);
+int arr[r][c];
+for(i=0;i<r;i++)
+for(j=0;j<c;j++)
+scanf("%d",&arr[i][j]);
+}
+for(i=0;i<r;i++)
+for(j=0;j<c;j++)
+printf("%d ",arr[i][j]);
